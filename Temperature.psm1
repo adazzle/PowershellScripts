12a544fef081f7f73ef54f814064a11c747b072a
aqilzeeshan <aqilzeeshan@users.noreply.github.com>
2013-09-17 17:39:47 +0100

    Simplified test functions

--- Temperature.psm1
+++ Temperature.psm1
@@ -1,33 +1,17 @@
 ## Convert Fahrenheit to Celcius
-
 function Convert-FToC([double] $fahrenheit)
-
 {
-
     $celcius = $fahrenheit - 32
-
     $celcius = $celcius / 1.8
-
     $celcius
-
 }
-
-
 
 ## Convert Celcius to Fahrenheit
-
 function Convert-CToF([double] $celcius)
-
 {
-
     $fahrenheit = $celcius * 1.8
-
     $fahrenheit = $fahrenheit + 32
-
     $fahrenheit
-
 }
-
-
 
 Export-ModuleMember -Function Convert-FToC,Convert-CToF
