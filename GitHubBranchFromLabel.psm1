f0e6442e6ef6f5d06c7b4085fb97a9e0af98ade3
Steven Baker <steven.baker@adazzle.com>
2013-09-23 14:47:30 +0100

    Update GitHubBranchFromLabel.psm1

--- GitHubBranchFromLabel.psm1
+++ GitHubBranchFromLabel.psm1
@@ -1,9 +1,55 @@
 #Requires -Version 3.0
 
-function New-GithubBranchFromLabel
-{
-  $thisName = $MyInvocation.MyCommand.Name
-  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+function New_GitHubBranchFromLabel {
+[CmdletBinding()]
+param(
+[Parameter(Mandatory = $true)]
+[string]
+$UserName,
+
+[Parameter(Mandatory = $true)]
+[string]
+$Password,
+
+
+[Parameter(Mandatory = $true)]
+[string]
+$TempBranchName,
+
+[Parameter(Mandatory = $true)]
+[string]
+$RepoOwner,
+
+[Parameter(Mandatory = $true)]
+[string]
+$Repo,
+
+
+[Parameter(Mandatory = $false)]
+[string]
+$Labels = "Ready",
+
+
+[Parameter(Mandatory = $false)]
+[switch]
+$ForceTempBranch = $true,
+
+[Parameter(Mandatory = $false)]
+[switch]
+$PushToGitHub = $false,
+
+[Parameter(Mandatory = $false)]
+[switch]
+$InstallPreReqs = $true,
+
+[Parameter(Mandatory = $false)]
+[string]
+$BaseBranch = "dev"
+)
+
+
+
+  Write-Host "All Done"
 }
 
 Export-ModuleMember -Function  New-GithubBranchFromLabel
