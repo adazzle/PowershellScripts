3dce5f8d357e1c357784984b3725e346154536c1
Aqil <aqil.zeeshan@adazzle.com>
2013-11-12 18:06:55 +0000

    Creating Pull Request and Adding comment to Jira issue

--- Github-Jira-interaction.ps1
+++ Github-Jira-interaction.ps1
@@ -1,25 +1,28 @@
 if (2 -ge $host.version.major) { "Get http://chocolatey.org/ and cinst powershell" } else { "Powershell 3 already installed" }
 
 Write-Host "install PsGet if it is not installed"
-if(-not(Get-Module -name "PsGet")) {
-	Write-Host "PsGet is not installed so install it" 
-	(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex 
+if(-not(Get-Module -name "Posh-Github")) {
+	Write-Host "Posh-Github is not installed so install it" 
+	cinst Posh-GitHub
 }else{
 	Write-Host "PsGet is installed"
 }
 
-Write-Host "Install Posh-Github module"
-Install-Module -ModuleUrl "https://raw.github.com/Iristyle/Posh-GitHub/master/Posh-Github.psm1"
-
-
 $UserName=[environment]::GetEnvironmentVariable("github_username","User")
 $Password=[environment]::GetEnvironmentVariable("github_password","User")
-
 New-GitHubOAuthToken -Username $UserName -Password $Password
 Write-Host $GITHUB_API_OUTPUT
 
 Write-Host "Create new PR"
 New-GitHubPullRequest -Title 'Automated PR creation testing' -Body 'More detail' -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln"
 
-
-
+$JiraUserName = [environment]::GetEnvironmentVariable("jira_username","User")
+$JiraPassword = [environment]::GetEnvironmentVariable("jira_password","User")
+$headers = @{Authorization="Basic " + [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("${JiraUserName}:$JiraPassword"))}
+[Uri]$uri = "https://adazzle.atlassian.net/rest/api/2/issue/AD-1763/comment"
+$issue = ConvertTo-Json -InputObject @('"body": "This is a comment I want to post."')
+ $issue = @{
+		body = "This is an automated comment"
+	} | ConvertTo-Json
+Write-Host $issue
+Invoke-RestMethod -Uri $uri -Headers ($headers) -Method Post -Body $issue -ContentType "application/json"
