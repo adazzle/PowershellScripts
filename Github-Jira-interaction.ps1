08f48f07a5ccae44122573da8c293d92ca39bd37
Aqil <aqil.zeeshan@adazzle.com>
2013-11-13 13:55:11 +0000

    Overall control flow defined - Github PR and Commit links going to Jira issue as comment

--- Github-Jira-interaction.ps1
+++ Github-Jira-interaction.ps1
@@ -1,28 +1,53 @@
-if (2 -ge $host.version.major) { "Get http://chocolatey.org/ and cinst powershell" } else { "Powershell 3 already installed" }
+if (2 -ge $host.version.major) { 
+	cinst powershell
+}else{ 
+	"Powershell 3 already installed" 
+}
 
-Write-Host "install PsGet if it is not installed"
+Write-Host "install Posh-Github if it is not installed"
 if(-not(Get-Module -name "Posh-Github")) {
 	Write-Host "Posh-Github is not installed so install it" 
 	cinst Posh-GitHub
 }else{
-	Write-Host "PsGet is installed"
+	Write-Host "Posh-Github is installed"
 }
 
-$UserName=[environment]::GetEnvironmentVariable("github_username","User")
-$Password=[environment]::GetEnvironmentVariable("github_password","User")
-New-GitHubOAuthToken -Username $UserName -Password $Password
-Write-Host $GITHUB_API_OUTPUT
+$AnswerPRneeded = Read-Host "Confirm - there is no existing PR (y/n)"
+while("y","n" -notcontains $AnswerPRneeded)
+{
+	$AnswerPRneeded = Read-Host "Confirm - there is no existing PR (y/n)"
+}
 
-Write-Host "Create new PR"
-New-GitHubPullRequest -Title 'Automated PR creation testing' -Body 'More detail' -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln"
+if ($AnswerPRneeded -eq "y") 
+{
+	$JiraIssue = Read-Host "Specify Jira issue no:(example: AD-1234)"
+	if ($JiraIssue)
+	{
+		$UserName=[environment]::GetEnvironmentVariable("github_username","User")
+		$Password=[environment]::GetEnvironmentVariable("github_password","User")
+		New-GitHubOAuthToken -Username $UserName -Password $Password
+		Write-Host $GITHUB_API_OUTPUT
 
-$JiraUserName = [environment]::GetEnvironmentVariable("jira_username","User")
-$JiraPassword = [environment]::GetEnvironmentVariable("jira_password","User")
-$headers = @{Authorization="Basic " + [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("${JiraUserName}:$JiraPassword"))}
-[Uri]$uri = "https://adazzle.atlassian.net/rest/api/2/issue/AD-1763/comment"
-$issue = ConvertTo-Json -InputObject @('"body": "This is a comment I want to post."')
- $issue = @{
-		body = "This is an automated comment"
-	} | ConvertTo-Json
-Write-Host $issue
-Invoke-RestMethod -Uri $uri -Headers ($headers) -Method Post -Body $issue -ContentType "application/json"
+		Write-Host "Create Pull Request"
+		$GITHUB_API_OUTPUT=""
+		New-GitHubPullRequest -Title 'Automated PR creation testing' -Body 'More detail' -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln"	
+		
+		#Get-GitHubPullRequests -Owner "adazzle" -Repository "mediaAppSln" -State open
+		if($GITHUB_API_OUTPUT)
+		{
+			$JiraUserName = [environment]::GetEnvironmentVariable("jira_username","User")
+			$JiraPassword = [environment]::GetEnvironmentVariable("jira_password","User")
+			$headers = @{Authorization="Basic " + [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("${JiraUserName}:$JiraPassword"))}
+			$html_url = "PR:" + ($GITHUB_API_OUTPUT | Select -ExpandProperty html_url)
+			$commits_url = "Commits:" + ($GITHUB_API_OUTPUT | Select -ExpandProperty commits_url)
+			[Uri]$uri = "https://adazzle.atlassian.net/rest/api/2/issue/$JiraIssue/comment"
+			 $issue = @{
+					body = "$html_url $commits_url"
+				} | ConvertTo-Json
+			#Write-Host $issue
+			Invoke-RestMethod -Uri $uri -Headers ($headers) -Method Post -Body $issue -ContentType "application/json"
+		}
+	}else{
+		Write-Host "With no Jira issue provided to create link between PR and Jira issue, this helper is not very useful so Please create PR in Github"
+	}
+}
