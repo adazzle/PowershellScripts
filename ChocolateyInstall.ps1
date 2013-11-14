24b051d67bebecc39a159e0fbe04ffb05d9a7bfc
Aqil <aqil.zeeshan@adazzle.com>
2013-11-14 15:22:51 +0000

    Powershell script for Chocolatey to install/copy Git helpers to right place

---
+++ ChocolateyInstall.ps1
@@ -0,0 +1,9 @@
+######## To do list ######## 
+
+# Download git helpers as zip file
+
+# Extract git helpers to a temp folder
+
+# Find destination directory of git helpers through git --exec-path
+
+# Copy git helpers to destination directory
