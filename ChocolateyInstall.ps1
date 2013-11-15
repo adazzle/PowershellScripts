3fcc01777e3b27d043c393ae3c48ba39869a52e2
Aqil <aqil.zeeshan@adazzle.com>
2013-11-15 12:12:16 +0000

    Downloads and Copy git helper to desired destination

--- ChocolateyInstall.ps1
+++ ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
-######## To do list ######## 
+######## It does following tasks: ######## 
 
 # Download git helpers as zip file
 
@@ -7,3 +7,36 @@
 # Find destination directory of git helpers through git --exec-path
 
 # Copy git helpers to destination directory
+
+# variables
+$url = "https://github.com/adazzle/PowershellScripts/blob/b95531ec6bd4b5bbf40483ee52e2b1a926a091ae/"
+$githelpersTempDir = Join-Path $env:TEMP "githelpers"
+$tempDir = Join-Path $githelpersTempDir "githelpersInstall"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$file = Join-Path $tempDir "git-create-pr.zip"
+
+function Download-File {
+param (
+  [string]$url,
+  [string]$file
+ )
+  Write-Host "Downloading $url to $file"
+  $downloader = new-object System.Net.WebClient
+  $downloader.DownloadFile($url, $file)
+}
+
+# download the package
+Download-File 'https://github.com/adazzle/PowershellScripts/blob/b95531ec6bd4b5bbf40483ee52e2b1a926a091ae/git-create-pr.zip?raw=true' $file
+
+# download 7zip
+Write-Host "Download 7Zip commandline tool"
+$7zaExe = Join-Path $tempDir '7za.exe'
+Download-File 'https://github.com/chocolatey/chocolatey/blob/master/src/tools/7za.exe?raw=true' "$7zaExe"
+
+# unzip the package
+Write-Host "Extracting $file to $tempDir..."
+Start-Process "$7zaExe" -ArgumentList "x -o`"$tempDir`" -y `"$file`"" -Wait
+
+#Get destination folder
+$destination = git --exec-path
+Write-Host "Copying 'git create-pr helper' to $destination"
