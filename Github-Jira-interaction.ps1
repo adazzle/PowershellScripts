ea50748bea0a2e49e60d8a66481ab980d2c94e3d
Aqil <aqil.zeeshan@adazzle.com>
2013-11-18 13:44:21 +0000

    Environment variables are created by powershell and this is one off

--- Github-Jira-interaction.ps1
+++ Github-Jira-interaction.ps1
@@ -6,32 +6,51 @@ function Install-Prerequisite()
 		"Powershell 3 already installed" 
 	}
 
-	if(-not(Get-Module -name "Posh-Github")) {
+	if($(Get-Module -ListAvailable | Where-Object { $_.name -eq "Posh-Github" }))
+	{
+		Write-Host "Posh-Github is installed" 
+	}else{
 		Write-Host "Posh-Github is not installed so install it" 
 		cinst Posh-GitHub
-	}else{
-		Write-Host "Posh-Github is installed"
 	}
   return $null
 }
 
-# Install powershell 3 and Posh-Github if not already installed
-Install-Prerequisite
-
-$AnswerPRneeded = Read-Host "Confirm - there is no existing PR (y/n)"
-while("y","n" -notcontains $AnswerPRneeded)
+function Initialize-JiraGithubCredentials()
 {
-	$AnswerPRneeded = Read-Host "Confirm - there is no existing PR (y/n)"
+	Param([string]$envariable,
+		  [string]$message)
+	
+	$envvalue = [environment]::GetEnvironmentVariable($envariable,"User")
+	if(!$envvalue)
+	{
+		$envvalue = Read-Host "Please provide $message "
+		[Environment]::SetEnvironmentVariable($envariable, $envvalue, "User")
+	}
+	return $envvalue
 }
 
-if ($AnswerPRneeded -eq "y") 
+# Install powershell 3 and Posh-Github if not already installed
+Install-Prerequisite
+# Git\Set Github and Jira username\passwords
+$JiraUserName = Initialize-JiraGithubCredentials -envariable "jira_username" -message "Jira username"
+$JiraPassword = Initialize-JiraGithubCredentials -envariable "jira_password" -message "Jira password"
+$UserName = Initialize-JiraGithubCredentials -envariable "github_username" -message "Github username"
+$Password = Initialize-JiraGithubCredentials -envariable "github_password" -message "Github password"
+
+$JiraBaseUrl = "https://adazzle.atlassian.net"
+
+$AnswerPRneeded = Read-Host "Do you have any existing PR to dev for this branch?(y/n)"
+while("y","n" -notcontains $AnswerPRneeded)
+{
+	$AnswerPRneeded = Read-Host "Do you have any existing PR to dev for this branch?(y/n)"
+}
+
+if ($AnswerPRneeded -eq "n") 
 {
 	$JiraIssue = Read-Host "Specify Jira issue no(example: AD-1234)"
 	if ($JiraIssue)
 	{
-		$JiraBaseUrl = "https://adazzle.atlassian.net"
-		$JiraUserName = [environment]::GetEnvironmentVariable("jira_username","User")
-		$JiraPassword = [environment]::GetEnvironmentVariable("jira_password","User")
 		$headers = @{Authorization="Basic " + [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("${JiraUserName}:$JiraPassword"))}
 		
 		[Uri]$uri = "$JiraBaseUrl/rest/api/2/issue/$JiraIssue/"
@@ -45,8 +64,6 @@ if ($AnswerPRneeded -eq "y")
 			$summary = ($result | Select -ExpandProperty fields) | Select -ExpandProperty summary
 			#Write-Host "$summary"
 
-			$UserName=[environment]::GetEnvironmentVariable("github_username","User")
-			$Password=[environment]::GetEnvironmentVariable("github_password","User")
 			New-GitHubOAuthToken -Username $UserName -Password $Password
 			New-GitHubPullRequest -Title "[$JiraIssue] - $summary" -Body "$JiraBaseUrl/browse/$JiraIssue" -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln"	
 			
@@ -63,6 +80,8 @@ if ($AnswerPRneeded -eq "y")
 		Write-Host "This is not a valid Jira issue"
 		}
 	}else{
-		Write-Host "With no Jira issue provided, this helper is useless so Please create PR in Github"
+		Write-Host "With no Jira issue provided, this git helper is useless so Please create PR in Github."
 	}
+}else{
+	Write-Host "This git helper cannot create another PR."
 }
