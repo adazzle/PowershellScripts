3d80152e2b7b348c648ca2f6c1b06a7a95e1c86b
Aqil <aqil.zeeshan@adazzle.com>
2013-11-26 17:05:56 +0000

    Script to establish tracebility between Github pull requests and Jira issues

---
+++ Github-Jira-interaction.ps1
@@ -0,0 +1,113 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+function Initialize-JiraGithubCredentials()
+{
+	Param([string]$envariable,
+		  [string]$message)
+	
+	$envvalue = [environment]::GetEnvironmentVariable($envariable,"User")
+	if(!$envvalue)
+	{
+		$envvalue = Read-Host "Please provide $message "
+		[Environment]::SetEnvironmentVariable($envariable, $envvalue, "User")
+	}
+	return $envvalue
+}
+
+#insufficient Posh-Github function Get-GitHubPullRequests so have to create our own
+function Find-ExistingPullRequest()
+{
+	Param([string]$Base,
+		  [string]$Owner,
+		  [string]$Repository,
+		  [string]$State)
+		  
+	$totalCount = 0
+	
+	#get head
+	$localUser = git remote -v show |
+      ? { $_ -match 'origin\t.*github.com\/(.*)\/.* \((fetch|push)\)' } |
+      % { $matches[1] } |
+      Select -First 1
+
+    $branchName = git symbolic-ref -q HEAD |
+      % { $_ -replace 'refs/heads/', ''}
+
+    $Head = "$($localUser):$($branchName)"
+	
+	#Write-Host "Getting $State pull requests for $Owner/$Repository from $head to $base"
+	$uri = "https://api.github.com/repos/$Owner/$Repository/pulls?access_token=${Env:\GITHUB_OAUTH_TOKEN}&base=$Base&head=$Head&state=$State"
+	$global:GITHUB_API_OUTPUT = Invoke-RestMethod -Uri $uri
+	
+	$global:GITHUB_API_OUTPUT |
+    % {
+      $totalCount++
+      $created = [DateTime]::Parse($_.created_at)
+      $updated = [DateTime]::Parse($_.updated_at)
+      $open = ([DateTime]::Now - $created).ToString('%d')
+      Write-Host "`n$($_.number) from $($_.user.login) - $($_.title) "
+      Write-Host "`tOpen for $open day(s) / Last Updated - $($updated.ToString('g'))"
+      Write-Host "`t$($_.issue_url)"
+    }
+	Write-Host "`nFound $totalCount $State pull requests for $Owner/$Repository"
+	return $totalCount
+}
+
+# Git\Set Github and Jira username\passwords
+$JiraUserName = Initialize-JiraGithubCredentials -envariable "jira_username" -message "Jira username"
+$JiraPassword = Initialize-JiraGithubCredentials -envariable "jira_password" -message "Jira password"
+
+$JiraBaseUrl = "https://adazzle.atlassian.net"
+
+
+$GithubToken = [environment]::GetEnvironmentVariable("GITHUB_OAUTH_TOKEN","User")
+if (!$GithubToken){
+	$UserName = Read-Host "Please provide Github username "
+	$Password = Read-Host "Please provide Github password "
+	New-GitHubOAuthToken -Username $UserName -Password $Password
+}else{
+	$env:GITHUB_OAUTH_TOKEN = $GithubToken
+}
+	
+$TotalExistingPRs = Find-ExistingPullRequest -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln" -State "open"
+if ($TotalExistingPRs -eq "0") 
+{
+	Write-Host "Create new PR"
+	$JiraIssue = Read-Host "Specify Jira issue no(example: AD-1234)"
+	if ($JiraIssue)
+	{
+		$headers = @{Authorization="Basic " + [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("${JiraUserName}:$JiraPassword"))}
+		
+		[Uri]$uri = "$JiraBaseUrl/rest/api/2/issue/$JiraIssue/"
+		#Write-Host $issue
+		try{
+			$result=Invoke-RestMethod -Uri $uri -Headers ($headers) -ContentType "application/json"
+		}catch{}
+
+		if($result){
+			#Write-Host "Get title for PR"
+			$summary = ($result | Select -ExpandProperty fields) | Select -ExpandProperty summary
+			#Write-Host "$summary"
+
+			New-GitHubPullRequest -Title "[$JiraIssue] - $summary" -Body "$JiraBaseUrl/browse/$JiraIssue" -Base 'dev' -Owner "adazzle" -Repository "mediaAppSln"	
+			
+			$html_url = "PR:" + ($GITHUB_API_OUTPUT | Select -ExpandProperty html_url)
+			$commits_url = "Commits:" + ($GITHUB_API_OUTPUT | Select -ExpandProperty commits_url)
+			
+			[Uri]$uri = "$JiraBaseUrl/rest/api/2/issue/$JiraIssue/comment"
+			 $issue = @{
+					body = "$html_url $commits_url"
+				} | ConvertTo-Json
+			#Write-Host $issue
+			Write-Host "Going to create PR link Jira issue"
+			$result=Invoke-RestMethod -Uri $uri -Headers ($headers) -Method Post -Body $issue -ContentType "application/json"
+			Write-Host "PR link created in Jira issue"
+		}else{
+		Write-Host "This is not a valid Jira issue"
+		}
+	}else{
+		Write-Host "With no Jira issue provided, this git helper is useless so Please create PR in Github."
+	}
+}else{
+	Write-Host "One PR already exists - another cannot be created."
+}
